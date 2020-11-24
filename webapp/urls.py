b5a35586bd9f34cba3d816a21d97893c968f67c6
n-guitar <nguitar001@gmail.com>
2020-11-24 21:41:20 +0900

    websocket with async view

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AsuncIndex
 
 urlpatterns = [
     path('', IndexView.as_view() , name='IndexView' ),
+    path('async/', AsuncIndex, name='AsyncIndex' ),
 ]
