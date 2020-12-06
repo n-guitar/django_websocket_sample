92b840c9512be4e1ebe2d6daadcb341cb2217047
n-guitar <nguitar001@gmail.com>
2020-12-06 23:14:56 +0900

    echo するwebsocket server&任意のviewでwebsocket処理を行う
    - websocket/websocket.py
    resolveを使い任意のpathでwebsoket serverを指定できるようにする
    
    - websocket/connection.py
    websocketのaccept,send,receive等を共通処理化
    
    - webapp/views.py
    websocketの受け皿
    
    - templates/Index.html
    websocket serverのpathを変更

--- webapp/views.py
+++ webapp/views.py
@@ -5,7 +5,10 @@ from django.views import generic
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
 
-
 async def websocket_view(socket):
     await socket.accept()
-    await socket.send
+    await socket.send_text("socket start")
+
+    while True:
+        message = await socket.receive_text()
+        await socket.send_text('hello')
--- websocket/websocket.py
+++ websocket/websocket.py
@@ -1,13 +1,13 @@
+from django.urls import resolve
+from .connection import WebSocket
+
 def websockets(django_application):
     async def asgi(scope, receive, send):
         # debug
         print('----websocket.py:websockets----')
-        print('scope[\'type\']:',scope['type'])
-        print('scope[\'asgi\']:',scope['asgi'])
         print('scope[\'server\']:',scope['server'])
         print('scope[\'client\']:',scope['client'])
         print('scope[\'scheme\']:',scope['scheme'])
-        print('scope[\'root_path\']:',scope['root_path'])
         print('scope[\'path\']:',scope['path'])
         print('scope[\'raw_path\']:',scope['raw_path'])
         print('scope[\'query_string\']:',scope['query_string'])
@@ -19,32 +19,10 @@ def websockets(django_application):
             return
             # websocketのものはすべて『websocket_application』になり、js側がどんなpathでアクセスしてきてもすべて同じwebsocketで受け付けることになる
         elif scope["type"] == "websocket":
-            await websocket_application(scope, receive, send)
+            match = resolve(scope["raw_path"])
+            await match.func(WebSocket(scope, receive, send), *match.args, **match.kwargs)
+            # await websocket_application(scope, receive, send)
             return
         else:
             raise NotImplementedError(f"Unknown scope type {scope['type']}")
     return asgi
-
-async def websocket_application(scope, receive, send):
-    # scopeに任意のket:valueを追加可能
-    scope['room'] = 'test'
-    while True:
-        event = await receive()
-        # debug
-        print("----websocket.py----")
-        print(event)
-        print("scope/['room']:", scope['room'])
-
-        if event['type'] == 'websocket.connect':
-            await send({
-                'type': 'websocket.accept',
-            })
-
-        if event['type'] == 'websocket.disconnect':
-            break
-
-        if event['type'] == 'websocket.receive':
-            await send({
-                'type': 'websocket.send',
-                'text': event['text']
-            })
